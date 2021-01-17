d8b71c16b8659a481a71ef7a3329c0d7c9b1a4e8
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2021-01-17 16:04:38 +0700

    Transfer to typescript and refactor

---
+++ Chap-4/src/spider.ts
@@ -0,0 +1,57 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as superagent from "superagent";
+import * as mkdirp from "mkdirp";
+import { urlToFilename } from "./utils";
+
+function saveFile(
+  filename: string,
+  content: string,
+  cb: (err: Error | null) => void
+) {
+  mkdirp(path.dirname(filename))
+    .then(() => {
+      fs.writeFile(filename, content, cb);
+    })
+    .catch((err) => {
+      cb(err);
+    });
+}
+
+function download(
+  url: string,
+  filename: string,
+  cb: (err: Error | null, content?: string) => void
+) {
+  superagent.get(url).end((err, res) => {
+    if (err) {
+      return cb(err);
+    }
+    saveFile(filename, res.text, (err) => {
+      if (err) {
+        return cb(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      cb(null, res.text);
+    });
+  });
+}
+
+export function spider(
+  url: string,
+  cb: (err: Error | null, filename?: string, downloaded?: boolean) => void
+) {
+  const filename = urlToFilename(url);
+  fs.access(filename, (err) => {
+    if (!err || err.code !== "ENOENT") {
+      return cb(null, filename, false);
+    }
+    download(url, filename, (err) => {
+      if (err) {
+        return cb(err);
+      }
+
+      cb(null, filename, true);
+    });
+  });
+}
